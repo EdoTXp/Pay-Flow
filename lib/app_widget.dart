046f541dde90d50ac531a9390233a2a185629e1d
Edoardo Fabrizio De Iovanna <edoardofabriziodeiovanna@hotmail.com>
2021-06-24 14:27:55 -0300

    Adicionando a tela de scanner dos boletos.

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:pay_flow/modules/barcode_scanner/barcode_scanner_page.dart';
+import 'package:pay_flow/modules/insert_boleto/insert_boleto_page.dart';
 import 'package:pay_flow/modules/login/login_page.dart';
 
 import 'modules/home/home_page.dart';
@@ -6,16 +9,28 @@ import 'modules/splash/splash_page.dart';
 import 'shared/themes/app_colors.dart';
 
 class AppWidget extends StatelessWidget {
+  AppWidget() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pay Flow',
-      theme: ThemeData(primaryColor: AppColors.primary),
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        primaryColor: AppColors.primary,
+      ),
       initialRoute: "/splash",
       routes: {
         "/splash": (context) => SplashPage(),
         "/home": (context) => HomePage(),
-        "/login": (context) => LoginPage()
+        "/login": (context) => LoginPage(),
+        "/barcode_scanner": (context) => BarcodeScannerPage(),
+        "/insert_boleto": (context) => InsertBoletoPage()
       },
     );
   }
---
+++ lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:pay_flow/modules/barcode_scanner/barcode_scanner_controller.dart';
+import 'package:pay_flow/modules/barcode_scanner/barcode_scanner_status.dart';
+import 'package:pay_flow/shared/themes/app_colors.dart';
+import 'package:pay_flow/shared/themes/app_text_styles.dart';
+import 'package:pay_flow/shared/widgets/bottom_sheet/bottom_sheet_widget.dart';
+import 'package:pay_flow/shared/widgets/set_label_buttons/set_label_buttons.dart';
+
+class BarcodeScannerPage extends StatefulWidget {
+  const BarcodeScannerPage({Key? key}) : super(key: key);
+
+  @override
+  _BarcodeScannerPageState createState() => _BarcodeScannerPageState();
+}
+
+class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
+  final controller = BarcodeScannerController();
+
+  @override
+  void initState() {
+    controller.getAvailableCameras();
+    controller.statusNotifier.addListener(() {
+      if(controller.status.hasBarcode)
+        Navigator.pushNamed(context, "/insert_boleto");
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      top: true,
+      bottom: true,
+      left: true,
+      right: true,
+      child: Stack(
+        children: [
+          ValueListenableBuilder<BarcodeScannerStatus>(
+              valueListenable: controller.statusNotifier,
+              builder: (_, status, __) {
+                if (status.showCamera) {
+                  return Container(
+                    child: status.cameraController!.buildPreview(),
+                  );
+                } else
+                  return Container();
+              }),
+          RotatedBox(
+            quarterTurns: 1,
+            child: Scaffold(
+              backgroundColor: Colors.transparent,
+              appBar: AppBar(
+                backgroundColor: Colors.black,
+                title: Text(
+                  "Escaneie o código de barras do boleto",
+                  style: AppTextStyles.buttonBackground,
+                ),
+                centerTitle: true,
+                leading: BackButton(
+                  color: AppColors.background,
+                ),
+              ),
+              body: Column(
+                children: [
+                  Expanded(
+                      child: Container(
+                    color: Colors.black.withOpacity(0.6),
+                  )),
+                  Expanded(
+                      flex: 2,
+                      child: Container(
+                        color: Colors.transparent,
+                      )),
+                  Expanded(
+                      child: Container(
+                    color: Colors.black.withOpacity(0.6),
+                  ))
+                ],
+              ),
+              bottomNavigationBar: SetLabelButtons(
+                primaryLabel: "Inserir código do boleto",
+                primaryOnPressed: () {},
+                secondaryLabel: "Adicionar da galeria",
+                secondaryOnPressed: () {},
+              ),
+            ),
+          ),
+          ValueListenableBuilder<BarcodeScannerStatus>(
+              valueListenable: controller.statusNotifier,
+              builder: (_, status, __) {
+                if (status.hasError) {
+                  return BottomSheetWidget(
+                      primaryLabel: "Escanear novamente",
+                      primaryOnPressed: () {
+                        controller.getAvailableCameras();
+                      },
+                      secondaryLabel: "Digitar código",
+                      secondaryOnPressed: () {},
+                      title:
+                          "Não foi possível identificar um código de barras.",
+                      subtitle:
+                          "Tente escanear novamente ou digite o código do seu boleto");
+                } else
+                  return Container();
+              })
+        ],
+      ),
+    );
+  }
+}
